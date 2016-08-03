fff40e4e45119fbaa352c8f36c3fd0f8b04812f9
shensky711 <shensky711@qq.com>
2016-08-03 22:22:14 +0800

    refine disk lru cache

--- library/src/main/java/com/hanschen/easyloader/cache/LruDiskCache.java
+++ library/src/main/java/com/hanschen/easyloader/cache/LruDiskCache.java
@@ -1,22 +1,46 @@
 package com.hanschen.easyloader.cache;
 
+import com.hanschen.easyloader.cache.diskcache.DiskLruCache;
+import com.hanschen.easyloader.log.EasyLoaderLog;
+
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by Hans.Chen on 2016/8/2.
  */
-public class LruDiskCache<K, V> implements CacheManager<K, V> {
+public class LruDiskCache implements CacheManager<String, File> {
+
+    private static final String TAG = "LruDiskCache";
+
+    private static final int APP_VERSION = 1;
+    private static final int VALUE_COUNT = 1;
+
+    private DiskLruCache diskLruCache;
+
+    public LruDiskCache(File directory, int maxSize) {
+        if (diskLruCache == null) {
+            try {
+                diskLruCache = DiskLruCache.open(directory, APP_VERSION, VALUE_COUNT, maxSize);
+            } catch (IOException e) {
+                EasyLoaderLog.e(TAG, "can't open DiskLruCache");
+                e.printStackTrace();
+            }
+        }
+    }
 
     @Override
-    public V get(K key) {
+    public File get(String key) {
         return null;
     }
 
     @Override
-    public void put(K key, V value) {
+    public void put(String key, File value) {
 
     }
 
     @Override
-    public V remove(K key) {
+    public File remove(String key) {
         return null;
     }
 
---
+++ library/src/main/java/com/hanschen/easyloader/util/MD5Utils.java
@@ -0,0 +1,35 @@
+package com.hanschen.easyloader.util;
+
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.codec.digest.DigestUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by Hans.Chen on 2016/8/3.
+ */
+public class MD5Utils {
+
+    /**
+     * 获得字符串md5值，注意，不要使用{@link DigestUtils#md5Hex(String)}方法直接返回字符串，会报NoSuchMethodError异常，因为android内置的类没有这个方法
+     *
+     * @param input 需要加密的字符串
+     * @return 32位MD5值
+     */
+    public static String getMD5(String input) {
+        return new String(Hex.encodeHex(DigestUtils.md5(input)));
+    }
+
+    /**
+     * 获得文件md5值，注意，不要使用{@link DigestUtils#md5Hex(InputStream)}方法直接返回字符串，会报NoSuchMethodError异常，因为android内置的类没有这个方法
+     *
+     * @param file 需要加密的文件
+     * @return 32位MD5值
+     */
+    public static String getMD5(File file) throws IOException {
+        return new String(Hex.encodeHex(DigestUtils.md5(new FileInputStream(file))));
+    }
+}
