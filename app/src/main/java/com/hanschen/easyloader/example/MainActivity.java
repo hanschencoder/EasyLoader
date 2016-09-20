931beb28ec3652daabfe2baf70beb1a7cceb1e63
shensky711 <shensky711@qq.com>
2016-09-20 19:52:32 +0800

    just update to android studio 2.2

--- app/src/main/java/com/hanschen/easyloader/example/MainActivity.java
+++ app/src/main/java/com/hanschen/easyloader/example/MainActivity.java
@@ -1,4 +1,4 @@
-package com.hanschen.easyloader;
+package com.hanschen.easyloader.example;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.hanschen.easyloader.R;
 
 public class MainActivity extends AppCompatActivity {
 
